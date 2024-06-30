892b40bf36eac1d23fdf52839c6029876ffbe8ee
Rohit kumar <rohitkumar01498@gmail.com>
2024-06-30 16:43:13 +0530

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import Experience from './Sections/Experiance/Experience';
+// import Experience from './Sections/Experiance/Experience';
 import Hero from './Sections/Hero/Hero';
-import Navbar from './Sections/Navbar/Navbar';
+// import Navbar from './Sections/Navbar/Navbar';
 import Projects from './Sections/Projects/Projects';
-import Skills from './Sections/Skills/Skills';
+// import Skills from './Sections/Skills/Skills';
 import Contact from './Sections/contact/Contact';
 import Footer from './Sections/footer/Footer';
 
