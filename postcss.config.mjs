a6cd08c77346fe257ae8f26d505ddc2007ede463
Rohit kumar <rohitkumar01498@gmail.com>
2024-07-07 09:17:37 +0530

    revised

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,10 @@
+// postcss.config.mjs
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
   plugins: [
-    tailwindcss(),
+    tailwindcss,
     autoprefixer,
   ],
 };
