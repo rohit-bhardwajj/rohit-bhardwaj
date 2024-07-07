3261a9fabd224388f31e28c35bcd7573fb8848d8
Rohit kumar <rohitkumar01498@gmail.com>
2024-07-07 12:05:24 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 // src/tailwind.config.js
 module.exports = {
   corePlugins: {
-    preflight: true, // Disable preflight if you don't want Tailwind's base styles
+    preflight: false, // Disable preflight if you don't want Tailwind's base styles
   },
   content: [
     'index.html',
+    "./src/**/*.{js,ts,jsx,tsx}",
     './src/sections/Technologies/**/*.{jsx,js,ts,tsx}', // Ensure this path covers your Technology component
   ],
   theme: {
